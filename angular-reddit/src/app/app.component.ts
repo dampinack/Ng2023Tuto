eec0b6c390a536c30db27d5397eeabe0be9a5c24
Alfonso Llinas <llinasalfonso@gmail.com>
2023-04-30 18:50:30 -0400

    1st Git Update dampinack Ap 30 2023

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  articles: Article[];
+
+
+  constructor(){
+    this.articles = [
+
+      new Article('Angular', 'http://angular.io', 3),
+new Article('Fullstack', 'http://fullstack.io', 2),
+new Article('Angular Homepage', 'http://angular.io', 1),
+
+
+
+
+    ]
+  }
+
+
+
     // Objetos del type HTMLIputElement: title and link
     addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
-    
+
       // Por ello, se acceden por .value: usando backticks string (ES6)
     console.log("Salida Log:",`Adding article title: ${title.value} and link: ${link.value}`);
-    
+
+    // This is for proof:
+    alert("Hola Mundo, this is Javascript!");
+
     return false;
+
+
+
+
   }
-   
+
 }
---
+++ angular-reddit/src/app/article/article.model.ts
@@ -0,0 +1,38 @@
+
+// This is The Model (the article):
+export class Article{
+
+  title:  string;
+  link:   string;
+  votes:  number;
+
+  // The votes? means that is Optional!, default=0
+  constructor(title: string, link: string, votes?: number){
+
+    this.title = title;
+    this.link = link;
+    this.votes = votes || 0;
+  }
+
+  voteUp(): void{
+    this.votes += 1;
+  }
+
+  voteDown(): void{
+    this.votes -= 1;
+  }
+
+
+  domain():string{
+    try{
+
+      const domainAndPath: string = this.link.split('//')[1];
+
+      return domainAndPath.split('/')[0];
+
+    }catch(err){
+      return '0';
+    }
+  }
+
+}

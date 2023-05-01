f72e6e354c2eca6b3e820acaea133e15413616b1
Alfonso Llinas <llinasalfonso@gmail.com>
2023-05-01 17:47:08 -0400

    Git Update dampinack May 01 2023

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -8,37 +8,29 @@ import { Article } from './article/article.model';
 })
 export class AppComponent {
 
+  // an Array...pattern Generics
   articles: Article[];
 
 
-  constructor(){
+  constructor() {
     this.articles = [
 
       new Article('Angular', 'http://angular.io', 3),
-new Article('Fullstack', 'http://fullstack.io', 2),
-new Article('Angular Homepage', 'http://angular.io', 1),
-
-
-
-
-    ]
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
   }
 
+  // Objetos del type HTMLIputElement: title and link
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
 
-
-    // Objetos del type HTMLIputElement: title and link
-    addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean{
-
-      // Por ello, se acceden por .value: usando backticks string (ES6)
-    console.log("Salida Log:",`Adding article title: ${title.value} and link: ${link.value}`);
+    // Por ello, se acceden por .value: usando backticks string (ES6)
+    console.log("Salida Log:", `Adding article title: ${title.value} and link: ${link.value}`);
 
     // This is for proof:
-    alert("Hola Mundo, this is Javascript!");
+    //alert("Hola Mundo, this is Javascript!");
 
     return false;
-
-
-
 
   }
 
